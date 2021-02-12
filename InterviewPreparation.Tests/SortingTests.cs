844f80a7db588548aa1a765dd3f34bcd3ae6c430
samorodov <alexeysamorodov@gmail.com>
2021-02-12 12:42:40 +0500

    тесты на быструю сортировку

---
+++ InterviewPreparation.Tests/SortingTests.cs
@@ -0,0 +1,23 @@
+﻿using InterviewPreparation.Tasks;
+using NUnit.Framework;
+
+namespace InterviewPreparation.Tests
+{
+    [TestFixture]
+    public class SortingTests
+    {
+        [TestCase(new[] { 5, 1, 4, 2, 3 }, new[] { 1,2,3,4,5 })]
+        public void HoareSort_ShouldSortAndMatchRightExpected(int[] source, int[] expected)
+        {
+            HoareSort.Sort(source, 0, source.Length - 1);
+            Assert.AreEqual(source, expected);
+        }
+
+        [TestCase(new[] { 5, 1, 4, 2, 3 }, new[] { 1, 2, 3, 5, 4 })]
+        public void HoareSort_ShouldNotMatchWrongSorting(int[] source, int[] expected)
+        {
+            HoareSort.Sort(source, 0, source.Length - 1);
+            Assert.AreNotEqual(source, expected);
+        }
+    }
+}

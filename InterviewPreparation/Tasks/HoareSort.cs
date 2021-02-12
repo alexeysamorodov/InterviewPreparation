251bb69a2eeafc6a08975308d6545211c90fda6b
samorodov <alexeysamorodov@gmail.com>
2021-02-12 12:42:21 +0500

    Quick sort

---
+++ InterviewPreparation/Tasks/HoareSort.cs
@@ -0,0 +1,28 @@
+﻿namespace InterviewPreparation.Tasks
+{
+    public static class HoareSort
+    {
+        public static void Sort(int[] arr, int start, int end)
+        {
+            if (start == end)
+                return;
+            var pivot = arr[end];
+            var storeIndex = start;
+            for (var i = start; i < end; i++)
+            {
+                if (arr[i] <= pivot)
+                {
+                    var t = arr[i];
+                    arr[i] = arr[storeIndex];
+                    arr[storeIndex] = t;
+                    storeIndex++;
+                }
+            }
+            var temp = arr[storeIndex];
+            arr[storeIndex] = pivot;
+            arr[end] = temp;
+            if (storeIndex > start) Sort(arr, start, storeIndex - 1);
+            if (storeIndex < end) Sort(arr, storeIndex + 1, end);
+        }
+    }
+}

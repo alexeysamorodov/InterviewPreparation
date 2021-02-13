4d4edec88ebcf0af6007931fc22ae751da5b1152
samorodov <alexeysamorodov@gmail.com>
2021-02-13 23:12:16 +0500

    задача MergeKSortedLists

---
+++ InterviewPreparation.Tests/MergeKSortedListsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using InterviewPreparation.Tasks;
+using NUnit.Framework;
+
+namespace InterviewPreparation.Tests
+{
+    [TestFixture]
+    public class MergeKSortedListsTests
+    {
+        [TestCase(new[]{1, 1, 2, 3, 4, 4, 5, 6},
+        new []{1, 4, 5}, new[] { 1, 3, 4}, new[] { 2, 6})]
+        public void MergeKSortedLists_ShouldMerge(int[] expected, params int[][] sourceArrays)
+        {
+            var lists = sourceArrays.Select(MergeKSortedLists.CreateLinkedList).ToArray();
+            var task = new MergeKSortedLists();
+            var actual = task.MergeKLists(lists).ToArray();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ InterviewPreparation/Tasks/MergeKSortedLists.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.Tasks
+{
+    public class MergeKSortedLists
+    {
+        public static ListNode CreateLinkedList(IEnumerable<int> collection)
+        {
+            ListNode head = null;
+            var current = head;
+            foreach (var item in collection)
+            {
+                if (head == null)
+                {
+                    head = current = new ListNode(item);
+                }
+                else
+                {
+                    current.next = new ListNode(item);
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+
+        public class ListNode 
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val=0, ListNode next=null) {
+                this.val = val;
+                this.next = next;
+            }
+
+            public int[] ToArray()
+            {
+                var result = new List<int>();
+                var current = this;
+                while (current != null)
+                {
+                    result.Add(current.val);
+                    current = current.next;
+                }
+                return result.ToArray();
+            }
+        }
+
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0)
+                return null;
+            if (lists.Length == 1)
+                return lists[0];
+            var head = GetMinNodeAndMovePointer(lists);
+            var current = head;
+            while (true)
+            {
+                var min = GetMinNodeAndMovePointer(lists);
+                if (min == null)
+                    break;
+                current.next = min;
+                current = min;
+            }
+            return head;
+        }
+
+        public ListNode GetMinNodeAndMovePointer(ListNode[] lists)
+        {
+            ListNode min = null;
+            var minIndex = -1;
+            for (var i = 0; i < lists.Length; i++)
+            {
+                if (lists[i] != null && (min == null || min.val > lists[i].val))
+                {
+                    min = lists[i];
+                    minIndex = i;
+                }
+            }
+            if (minIndex > -1)
+            {
+                lists[minIndex] = lists[minIndex].next;
+            }
+            return min;
+        }
+    }
+}
